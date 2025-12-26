732d739d97b8503d3600e06dc057d07b72e70651
thgmai <thiagogmai@hotmail.com>
2025-12-26 09:47:42 -0300

    Arquivos iniciais de Robôs Autônomos

---
+++ src/turtle_control_thiago/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtle_control_thiago'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='thiago',
+    maintainer_email='thiago@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'turtle_control = turtle_control_thiago.turtle_control:main'
+        ],
+    },
+)
---
+++ src/turtle_control_thiago/turtle_control_thiago/turtle_control.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from turtle_control_thiago.')
+
+
+if __name__ == '__main__':
+    main()
