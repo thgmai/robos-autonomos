c84c2abe756f4f0247f676c049a082b916e59abd
thgmai <thiagogmai@hotmail.com>
2025-12-28 11:01:00 -0300

    Controle de posição robô uniciclo implementado

--- src/turtle_control_thiago/setup.py
+++ src/turtle_control_thiago/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'turtle_control_thiago'
 
@@ -10,13 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='thiago',
-    maintainer_email='thiago@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='thiago.mai@edu.ufes.br',
+    description='Pacote de controle para turtlesim',
+    license='Apache License 2.0',
     extras_require={
         'test': [
             'pytest',
@@ -24,7 +27,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'turtle_control = turtle_control_thiago.turtle_control:main'
+            'turtle_control = turtle_control_thiago.turtle_control:main',
+            'goal_publisher = turtle_control_thiago.goal_publisher:main',
         ],
     },
 )
